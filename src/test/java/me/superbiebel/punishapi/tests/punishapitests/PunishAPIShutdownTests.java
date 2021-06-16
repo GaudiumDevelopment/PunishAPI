c30d6dda840a7e0e4c6ac1d76cba9233bbee6635
superbiebel <63414167+superbiebel@users.noreply.github.com>
2021-06-16 17:03:01 +0200

    removed unnecessary Assertions class

--- src/test/java/me/superbiebel/punishapi/tests/punishapitests/PunishAPIShutdownTests.java
+++ src/test/java/me/superbiebel/punishapi/tests/punishapitests/PunishAPIShutdownTests.java
@@ -4,15 +4,13 @@ import me.superbiebel.punishapi.SystemStatus;
 import me.superbiebel.punishapi.api.PunishAPI;
 import me.superbiebel.punishapi.exceptions.ShutDownException;
 import me.superbiebel.punishapi.exceptions.StartupException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PunishAPIShutdownTests {
     
@@ -22,7 +20,7 @@ class PunishAPIShutdownTests {
         PunishAPI api = new PunishAPI();
         api.startup();
         api.shutdown();
-        Assertions.assertSame(SystemStatus.DOWN, api.status());
+        assertSame(SystemStatus.DOWN, api.status());
     }
     @ParameterizedTest
     @ValueSource(ints = {1,2,3,4,5})
