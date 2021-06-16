6a2664d7ef36be014b83c62d985224f7f9520fdf
superbiebel <63414167+superbiebel@users.noreply.github.com>
2021-06-16 22:46:10 +0200

    made separate tests for the API

---
+++ src/test/java/me/superbiebel/punishapi/tests/punishapitests/PunishAPIShutdownTests.java
@@ -0,0 +1,49 @@
+package me.superbiebel.punishapi.tests.punishapitests;
+
+import me.superbiebel.punishapi.SystemStatus;
+import me.superbiebel.punishapi.api.PunishAPI;
+import me.superbiebel.punishapi.exceptions.ShutDownException;
+import me.superbiebel.punishapi.exceptions.StartupException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PunishAPIShutdownTests {
+    
+    @Test
+    @Execution(ExecutionMode.CONCURRENT)
+    void readyBooleanShutdownTestTest() throws ShutDownException, StartupException {
+        PunishAPI api = new PunishAPI();
+        api.startup();
+        api.shutdown();
+        assertSame(SystemStatus.DOWN, api.status());
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    @Execution(ExecutionMode.CONCURRENT)
+    void multipleShutdownTest(int times) throws ShutDownException, StartupException {
+        PunishAPI api = new PunishAPI();
+        api.startup();
+        api.shutdown();
+        for (int i = 1; i<=times;i++) {
+            Throwable thrown = assertThrows(ShutDownException.class, api::shutdown);
+            assertEquals(IllegalStateException.class, thrown.getCause().getClass());
+        }
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    @Execution(ExecutionMode.CONCURRENT)
+    void multipleKilledShutdownTest(int times) throws StartupException, ShutDownException {
+        PunishAPI api = new PunishAPI();
+        api.startup();
+        api.kill();
+        for (int i = 1; i<=times;i++) {
+            Throwable thrown = assertThrows(ShutDownException.class, api::kill);
+            assertEquals(IllegalStateException.class, thrown.getCause().getClass());
+        }
+    }
+}
---
+++ src/test/java/me/superbiebel/punishapi/tests/punishapitests/PunishAPIStartupTests.java
@@ -0,0 +1,39 @@
+package me.superbiebel.punishapi.tests.punishapitests;
+
+import me.superbiebel.punishapi.PunishCore;
+import me.superbiebel.punishapi.SystemStatus;
+import me.superbiebel.punishapi.exceptions.StartupException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PunishAPIStartupTests {
+    
+    //test if the boolean is
+    @Test
+    @Execution(ExecutionMode.CONCURRENT)
+    void readyBooleanStartupTest() throws StartupException {
+        PunishCore core = new PunishCore();
+        core.startup();
+        assertSame(SystemStatus.READY, core.status());
+    }
+    
+    // test if the api is started up twice, it should throw an exception.
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    @Execution(ExecutionMode.CONCURRENT)
+    void multipleStartupTest(int times) throws StartupException {
+        PunishCore core = new PunishCore();
+        core.startup();
+        for (int i = 1; i<=times;i++) {
+            Throwable thrown = assertThrows(StartupException.class, core::startup);
+            assertEquals(IllegalStateException.class, thrown.getCause().getClass());
+        }
+    }
+    
+    
+}
