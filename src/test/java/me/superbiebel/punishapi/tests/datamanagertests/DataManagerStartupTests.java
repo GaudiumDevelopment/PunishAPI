944118e73193496a227a82c548610526282d9707
superbiebel <63414167+superbiebel@users.noreply.github.com>
2021-06-16 17:06:54 +0200

    actually use datamanager instead of PunishCore in tests

--- src/test/java/me/superbiebel/punishapi/tests/datamanagertests/DataManagerStartupTests.java
+++ src/test/java/me/superbiebel/punishapi/tests/datamanagertests/DataManagerStartupTests.java
@@ -1,7 +1,7 @@
 package me.superbiebel.punishapi.tests.datamanagertests;
 
-import me.superbiebel.punishapi.PunishCore;
 import me.superbiebel.punishapi.SystemStatus;
+import me.superbiebel.punishapi.data.Datamanager;
 import me.superbiebel.punishapi.exceptions.StartupException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
@@ -17,9 +17,9 @@ class DataManagerStartupTests {
     @Test
     @Execution(ExecutionMode.CONCURRENT)
     void readyBooleanStartupTest() throws StartupException {
-        PunishCore api = new PunishCore();
-        api.startup();
-        assertSame(SystemStatus.READY, api.status());
+        Datamanager datamanager = new Datamanager();
+        datamanager.startup();
+        assertSame(SystemStatus.READY, datamanager.status());
     }
     
     // test if the api is started up twice, it should throw an exception.
@@ -27,10 +27,10 @@ class DataManagerStartupTests {
     @ValueSource(ints = {1,2,3,4,5})
     @Execution(ExecutionMode.CONCURRENT)
     void multipleStartupTest(int times) throws StartupException {
-        PunishCore api = new PunishCore();
-        api.startup();
+        Datamanager datamanager = new Datamanager();
+        datamanager.startup();
         for (int i = 1; i<=times;i++) {
-            Throwable thrown = assertThrows(StartupException.class, api::startup);
+            Throwable thrown = assertThrows(StartupException.class, datamanager::startup);
             assertEquals(IllegalStateException.class, thrown.getCause().getClass());
         }
     }
