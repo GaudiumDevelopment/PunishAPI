6ca573b3715f376a325f7162b8fc919ecda69fbb
superbiebel <63414167+superbiebel@users.noreply.github.com>
2021-06-17 18:12:51 +0200

    added getter

--- src/main/java/me/superbiebel/punishapi/offenseprocessing/dataobjects/Offense.java
+++ src/main/java/me/superbiebel/punishapi/offenseprocessing/dataobjects/Offense.java
@@ -1,8 +1,11 @@
 package me.superbiebel.punishapi.offenseprocessing.dataobjects;
 
+import lombok.Getter;
+
 import java.util.Map;
 import java.util.UUID;
 
+@Getter
 public class Offense {
     private UUID uuid;
     private long timeregistered;
