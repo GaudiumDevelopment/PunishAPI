5f037a0badfe31b44564d4157e7134b62cb41acf
superbiebel <63414167+superbiebel@users.noreply.github.com>
2021-06-21 11:32:37 +0200

    add service operation methods to OffenseAPI.java make OffenseManager.java extend ServiceRegistry.java and implement its methods.

--- src/main/java/me/superbiebel/punishapi/api/OffenseAPI.java
+++ src/main/java/me/superbiebel/punishapi/api/OffenseAPI.java
@@ -1,11 +1,24 @@
 package me.superbiebel.punishapi.api;
 
 import me.superbiebel.punishapi.PunishCore;
+import me.superbiebel.punishapi.exceptions.ServiceAlreadyRegisteredException;
+import me.superbiebel.punishapi.exceptions.ServiceNotFoundException;
+import me.superbiebel.punishapi.exceptions.ShutDownException;
+import me.superbiebel.punishapi.exceptions.StartupException;
+import me.superbiebel.punishapi.offenseprocessing.OffenseManager;
+import me.superbiebel.punishapi.services.Service;
 
 public class OffenseAPI {
     private final PunishCore core;
     
     public OffenseAPI(PunishCore core) {
         this.core = core;
+    }
+    
+    public void addService(OffenseManager.offenseProcessorType serviceType, Service service) throws StartupException, ServiceAlreadyRegisteredException {
+        core.getOffenseManager().addService(serviceType, service);
+    }
+    public void removeService(OffenseManager.offenseProcessorType serviceType, boolean kill) throws ShutDownException, ServiceNotFoundException {
+        core.getOffenseManager().removeService(serviceType,kill);
     }
 }
