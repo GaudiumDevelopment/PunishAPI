95eaa9fa2d8a2a3dddc9f78a33e9987335bf8ce4
Superbiebel <superbiebel@gmail.com>
2021-06-26 22:51:26 +0200

    made removeservice test

--- src/test/java/me/superbiebel/punishapi/tests/datamanagertests/DataManagerOperationTests.java
+++ src/test/java/me/superbiebel/punishapi/tests/datamanagertests/DataManagerOperationTests.java
@@ -3,23 +3,39 @@ package me.superbiebel.punishapi.tests.datamanagertests;
 import me.superbiebel.punishapi.PunishCore;
 import me.superbiebel.punishapi.SystemStatus;
 import me.superbiebel.punishapi.data.Datamanager;
+import me.superbiebel.punishapi.exceptions.ServiceAlreadyRegisteredException;
+import me.superbiebel.punishapi.exceptions.ServiceNotFoundException;
+import me.superbiebel.punishapi.exceptions.ShutDownException;
+import me.superbiebel.punishapi.exceptions.StartupException;
 import me.superbiebel.punishapi.tests.testobjects.TestServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class DataManagerOperationTests {
     
     
     @Test
-    void addServiceTest() throws Exception {
+    void addServiceTest() throws StartupException, ServiceAlreadyRegisteredException, ServiceNotFoundException {
         TestServiceImpl service = new TestServiceImpl();
         PunishCore punishCore = new PunishCore();
         punishCore.startup();
         punishCore.getDatamanager().addService(Datamanager.DataServiceType.TEST, service);
         assertSame(service, punishCore.getDatamanager().getService(Datamanager.DataServiceType.TEST));
-        TestServiceImpl serviceFromApi = (TestServiceImpl) punishCore.getDatamanager().getDataService(Datamanager.DataServiceType.TEST);
+        TestServiceImpl serviceFromApi = (TestServiceImpl) punishCore.getDatamanager().getService(Datamanager.DataServiceType.TEST);
         assertSame(SystemStatus.READY, serviceFromApi.status());
+    }
+    @ParameterizedTest
+    @ValueSource(booleans = {false,true})
+    void removeServiceTest(boolean kill) throws StartupException, ServiceAlreadyRegisteredException, ServiceNotFoundException, ShutDownException {
+        TestServiceImpl service = new TestServiceImpl();
+        PunishCore punishCore = new PunishCore();
+        punishCore.startup();
+        punishCore.getDatamanager().addService(Datamanager.DataServiceType.TEST, service);
+        TestServiceImpl removedService = (TestServiceImpl) punishCore.getDatamanager().removeService(Datamanager.DataServiceType.TEST, kill);
+        assertEquals(removedService.getStatus().get(), kill ? SystemStatus.KILLED : SystemStatus.DOWN);
     }
 }
