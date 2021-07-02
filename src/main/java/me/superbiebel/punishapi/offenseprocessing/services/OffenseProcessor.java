68b0bf01f5f2ec07c74f3138a55a01e0c4cc38db
Superbiebel <superbiebel@gmail.com>
2021-07-02 16:27:53 +0200

    add batch processing

--- src/main/java/me/superbiebel/punishapi/offenseprocessing/services/OffenseProcessor.java
+++ src/main/java/me/superbiebel/punishapi/offenseprocessing/services/OffenseProcessor.java
@@ -2,13 +2,12 @@ package me.superbiebel.punishapi.offenseprocessing.services;
 
 import me.superbiebel.punishapi.exceptions.FailedServiceOperationException;
 import me.superbiebel.punishapi.offenseprocessing.dataobjects.OffenseProcessingRequest;
-import me.superbiebel.punishapi.offenseprocessing.dataobjects.Punishment;
+import me.superbiebel.punishapi.offenseprocessing.dataobjects.OffenseRecord;
 import me.superbiebel.punishapi.services.Service;
 
-import java.util.List;
-
 public interface OffenseProcessor extends Service {
-    List<Punishment> process(OffenseProcessingRequest offenseProcessingRequest) throws FailedServiceOperationException;
+    OffenseRecord process(OffenseProcessingRequest offenseProcessingRequest) throws FailedServiceOperationException;
+    OffenseRecord[] process(OffenseProcessingRequest[] offenseProcessingRequest) throws FailedServiceOperationException;
     //returns true if this OffenseProcessor can handle a batch of criminal actions by the same person.
     boolean canHandleBatch();
 }
