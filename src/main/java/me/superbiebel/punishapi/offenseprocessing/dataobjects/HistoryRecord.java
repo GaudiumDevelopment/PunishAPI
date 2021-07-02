07d3ab4c7952bd7d2b7c3de5e298b237c9f124cd
Superbiebel <superbiebel@gmail.com>
2021-07-02 15:34:21 +0200

    put variables of HistoryRecord.java on top of the file as per java formatting rules

--- src/main/java/me/superbiebel/punishapi/offenseprocessing/dataobjects/HistoryRecord.java
+++ src/main/java/me/superbiebel/punishapi/offenseprocessing/dataobjects/HistoryRecord.java
@@ -9,6 +9,13 @@ import java.util.UUID;
 @Getter
 public class HistoryRecord {
     
+    protected UUID moderatorUUID;
+    protected UUID criminalUUID;
+    protected long timeregistered;
+    protected Map<String,String> attributes;
+    protected List<Offense> offensesDone; //list if a batch of offenses was done, otherwise just one.
+    protected List<Punishment> punishments;
+    
     public HistoryRecord(UUID moderatorUUID, UUID criminalUUID, long timeregistered, Map<String, String> attributes, List<Offense> offensesDone, List<Punishment> punishments) {
         this.moderatorUUID = moderatorUUID;
         this.criminalUUID = criminalUUID;
@@ -17,11 +24,4 @@ public class HistoryRecord {
         this.offensesDone = offensesDone;
         this.punishments = punishments;
     }
-    
-    protected UUID moderatorUUID;
-    protected UUID criminalUUID;
-    protected long timeregistered;
-    protected Map<String,String> attributes;
-    protected List<Offense> offensesDone; //list if a batch of offenses was done, otherwise just one.
-    protected List<Punishment> punishments;
 }
