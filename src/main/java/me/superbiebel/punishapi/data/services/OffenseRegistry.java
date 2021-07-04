4729d97fec4ad1df4b13550c6cca2c1adfb77a68
Superbiebel <superbiebel@gmail.com>
2021-07-04 19:54:56 +0200

    added retrieveOffense actually specified the generic

--- src/main/java/me/superbiebel/punishapi/data/services/OffenseRegistry.java
+++ src/main/java/me/superbiebel/punishapi/data/services/OffenseRegistry.java
@@ -1,8 +1,12 @@
 package me.superbiebel.punishapi.data.services;
 
+import me.superbiebel.punishapi.data.Datamanager;
 import me.superbiebel.punishapi.offenseprocessing.dataobjects.OffenseHistoryRecord;
 import me.superbiebel.punishapi.services.Service;
 
-public interface OffenseRegistry extends Service {
+import java.util.UUID;
+
+public interface OffenseRegistry extends Service<Datamanager.DataServiceType> {
     void storeOffense(OffenseHistoryRecord offenseHistoryRecord);
+    void retrieveOffense(UUID offenseUUID);
 }
