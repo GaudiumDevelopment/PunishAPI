4bcb94ce8a9482ed5b1cb936396161311f5fb2d3
Superbiebel <superbiebel@gmail.com>
2021-08-14 13:03:36 +0200

    extended documentation of @NotWriteFromScript

--- src/main/java/me/superbiebel/punishapi/offenseprocessing/annotations/NoWriteFromScript.java
+++ src/main/java/me/superbiebel/punishapi/offenseprocessing/annotations/NoWriteFromScript.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Nothing will happen if a script modifies a field with this annotation, the value inside that field will probably be overwritten. A field with this annotation must be regarded as a read only field.
+ * Nothing will happen if a script modifies a field with this annotation, the value inside that field will probably be overwritten. A field with this annotation must be regarded as a read only field from the POV of the script even though the final keyword inst there.
  */
 
 @Retention(RetentionPolicy.SOURCE)
