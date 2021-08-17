eaa64f5b4488e7dd02584d44ea8f59e97f1095b7
Superbiebel <superbiebel@gmail.com>
2021-08-17 17:53:53 +0200

    cleanup

--- src/main/java/me/superbiebel/punishapi/api/DataAPI.java
+++ src/main/java/me/superbiebel/punishapi/api/DataAPI.java
@@ -20,83 +20,83 @@ import org.jetbrains.annotations.Nullable;
 public class DataAPI {
     private final Datamanager datamanager;
 
-    protected DataAPI(PunishCore core) {
+    protected DataAPI(final PunishCore core) {
         core.setDataAPI(this);
         datamanager = core.getDatamanager();
     }
 
-    public void addService(Datamanager.DataServiceType dataServiceType, DataService service) throws StartupException, ServiceAlreadyRegisteredException {
+    public void addService(final Datamanager.DataServiceType dataServiceType, final DataService service) throws StartupException, ServiceAlreadyRegisteredException {
         datamanager.addService(dataServiceType, service);
     }
     
     @Nullable
-    public Service removeService(Datamanager.DataServiceType dataServiceType, boolean kill) throws ShutDownException, ServiceNotFoundException {
+    public Service removeService(final Datamanager.DataServiceType dataServiceType, final boolean kill) throws ShutDownException, ServiceNotFoundException {
         return datamanager.removeService(dataServiceType, kill);
     }
     
-    public Service getService(Datamanager.DataServiceType dataServiceType) throws ServiceNotFoundException {
+    public Service getService(final Datamanager.DataServiceType dataServiceType) throws ServiceNotFoundException {
         return datamanager.getService(dataServiceType);
     }
     
-    public OffenseHistoryRecord retrieveOffense(UUID offenseUUID) throws FailedDataOperationException {
+    public OffenseHistoryRecord retrieveOffense(final UUID offenseUUID) throws FailedDataOperationException {
         return datamanager.retrieveOffenseRecord(offenseUUID);
     }
     
-    public UserAccount retrieveUser(UUID userUUID) throws FailedDataOperationException {
+    public UserAccount retrieveUser(final UUID userUUID) throws FailedDataOperationException {
         return datamanager.retrieveUser(userUUID);
     }
     
-    public void setUserAttribute(UUID userUUID, String key, String value) throws FailedDataOperationException {
+    public void setUserAttribute(final UUID userUUID, final String key, final String value) throws FailedDataOperationException {
         datamanager.setUserAttribute(userUUID, key, value);
     }
     
-    public void removeUserAttribute(UUID userUUID,String key) throws FailedDataOperationException {
+    public void removeUserAttribute(final UUID userUUID, final String key) throws FailedDataOperationException {
         datamanager.removeUserAttribute(userUUID, key);
     }
     
-    public List<UserAccount> getUsersByAttribute(String key, String value) throws FailedDataOperationException {
+    public List<UserAccount> getUsersByAttribute(final String key, final String value) throws FailedDataOperationException {
         return datamanager.getUsersByAttribute(key, value);
     }
     
-    public List<UserAccount> getUsersByAttributeKey(String key) throws FailedDataOperationException {
+    public List<UserAccount> getUsersByAttributeKey(final String key) throws FailedDataOperationException {
         return datamanager.getUsersByAttributeKey(key);
     }
     
-    public List<UserAccount> getUsersByAttributeValue(String value) throws FailedDataOperationException {
+    public List<UserAccount> getUsersByAttributeValue(final String value) throws FailedDataOperationException {
         return datamanager.getUsersByAttributeValue(value);
     }
 
-    public boolean tryLockUser(UUID uuid) throws FailedDataOperationException {
+    public boolean tryLockUser(final UUID uuid) throws FailedDataOperationException {
         return datamanager.tryLockUser(uuid);
     }
 
     //The processing is done
-    public void unlockUser(UUID uuid) throws FailedDataOperationException{
+    public void unlockUser(final UUID uuid) throws FailedDataOperationException {
         datamanager.unlockUser(uuid);
     }
 
     //Checks if the user is locked
-    public boolean isUserLocked(UUID userUUID) throws FailedDataOperationException{
+    public boolean isUserLocked(final UUID userUUID) throws FailedDataOperationException {
         return datamanager.isUserLocked(userUUID);
     }
 
-    public void storeOffenseProcessingTemplate(OffenseProcessingTemplate template) throws FailedDataOperationException {
+    public void storeOffenseProcessingTemplate(final OffenseProcessingTemplate template) throws FailedDataOperationException {
         datamanager.storeOffenseProcessingTemplate(template);
     }
 
-    public OffenseProcessingTemplate retrieveOffenseProcessingTemplate(UUID templateUUID) throws FailedDataOperationException {
+    public OffenseProcessingTemplate retrieveOffenseProcessingTemplate(final UUID templateUUID) throws FailedDataOperationException {
         return datamanager.retrieveOffenseProcessingTemplate(templateUUID);
     }
 
-    public boolean deleteOffenseProcessingTemplate(UUID templateUUID) throws FailedDataOperationException {
+    public boolean deleteOffenseProcessingTemplate(final UUID templateUUID) throws FailedDataOperationException {
         return datamanager.deleteOffenseProcessingTemplate(templateUUID);
     }
 
-    public boolean updateOffenseProcessorUUIDInOffenseProcessingTemplate(UUID templateUUID, UUID newOffenseProcessorUUID) throws FailedDataOperationException {
+    public boolean updateOffenseProcessorUUIDInOffenseProcessingTemplate(final UUID templateUUID, final UUID newOffenseProcessorUUID) throws FailedDataOperationException {
         return datamanager.updateOffenseProcessorUUIDInOffenseProcessingTemplate(templateUUID, newOffenseProcessorUUID);
     }
 
-    public boolean updateScriptFile(UUID templateUUID, File newScriptFile) throws FailedDataOperationException {
+    public boolean updateScriptFile(final UUID templateUUID, final File newScriptFile) throws FailedDataOperationException {
         return datamanager.updateScriptFile(templateUUID, newScriptFile);
     }
 }
